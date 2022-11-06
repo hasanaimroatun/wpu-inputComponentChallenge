b32e71cbc71116c873450fce8daac61576ada9ec
hasanaimroatun <imroatunhasana@gmail.com>
2022-11-06 13:00:55 +0700

    import input and inputError

---
+++ src/components/Inputs.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <div class="mainTitle">Inputs</div>
+        <Input />
+        <InputError />
+    </div>
+</template>
+
+<script>
+import Input from './InputDefault.vue'
+import InputError from './InputError.vue'
+    export default {
+        name: 'Inputs-container',
+        components: {
+            Input,
+            InputError,
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
