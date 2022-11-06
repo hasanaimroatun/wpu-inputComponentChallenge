cfb1812ff17a400e3d38c85dd2aae724371a51cd
hasanaimroatun <imroatunhasana@gmail.com>
2022-11-06 13:01:59 +0700

    import font awesome support

--- src/main.js
+++ src/main.js
@@ -2,5 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import "../node_modules/bootstrap/dist/css/bootstrap.css"
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle"
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faCartPlus } from '@fortawesome/free-solid-svg-icons'
 
-createApp(App).mount('#app')
+library.add(faCartPlus)
+
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
