77b8d31e96df2cb9e415cafafc0e99645eb23874
hasanaimroatun <imroatunhasana@gmail.com>
2022-11-06 18:01:10 +0700

    import inputDisabled, inputFullWidth, inputHelperText, inputMultiline, inputSize, and inputText

--- src/components/Inputs.vue
+++ src/components/Inputs.vue
@@ -3,21 +3,48 @@
         <div class="mainTitle">Inputs</div>
         <Input />
         <InputError />
+        <InputDisabled />
+        <InputHelperText />
+        <InputText />
+        <InputSize />
+        <InputFullWidth />
+        <InputMultiline />
+        <div class="createdBy mb-5">created by <a href="#">hasanaimroatun</a> - devChallenges.io</div>
     </div>
 </template>
 
 <script>
 import Input from './InputDefault.vue'
 import InputError from './InputError.vue'
+import InputDisabled from './InputDisabled.vue'
+import InputHelperText from './InputHelperText.vue'
+import InputText from './InputText.vue'
+import InputSize from './InputSize.vue'
+import InputFullWidth from './InputFullWidth.vue'
+import InputMultiline from './InputMultiline.vue'
     export default {
         name: 'Inputs-container',
         components: {
             Input,
             InputError,
+            InputDisabled,
+            InputHelperText,
+            InputText,
+            InputSize,
+            InputFullWidth,
+            InputMultiline,
         }
     }
 </script>
 
-<style scoped>
+<style>
+input {
+    width: 200px;
+    height: 56px;
+    border-radius: 8px;
+}
 
+label {
+    margin-top: 16px;
+}
 </style>
