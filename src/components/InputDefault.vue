4410b52b7635213d60dff76e3914ab9d6647709b
hasanaimroatun <imroatunhasana@gmail.com>
2022-11-06 13:00:27 +0700

    add elements to template and export file

---
+++ src/components/InputDefault.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <small v-text="labelTitle"></small>
+        <div class="d-flex flex-column">
+            <label for="iDefault">Label</label>
+            <input type="text" name="iDefault" id="iDefault" placeholder="Placeholder">
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Input-default',
+        data() {
+            return {
+                labelTitle: '<Input />'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <div class="title"><span>Dev</span>challenges.io</div>
+        <div class="navBar">
+            <ul class="navbar-nav pe-3">
+                <li class="nav-item mb-3">
+                    <a class="nav-link" href="#">Typography</a>
+                </li>
+                <li class="nav-item mb-3">
+                    <a class="nav-link" href="#">Grid</a>
+                </li>
+                <li class="nav-item mb-3">
+                    <a class="nav-link" href="#">Buttons</a>
+                </li>
+                <li class="nav-item mb-3">
+                    <a class="nav-link" href="#"><b>Inputs</b></a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Navigation-container"
+    }
+</script>
+
+<style scoped>
+
+
+</style>
