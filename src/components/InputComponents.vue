c1e22106ae2f0173554684f1540eb6c7a7dc68d6
hasanaimroatun <imroatunhasana@gmail.com>
2022-11-06 12:59:39 +0700

    import navigation and inputs

--- src/components/InputComponents.vue
+++ src/components/InputComponents.vue
@@ -1,15 +1,27 @@
 <template>
-    <div>
-
+    <div class="row">
+        <div class="col-2 offset-1 text-start">
+            <Navigation />
+        </div>
+        <div class="col-9 text-start">
+            <Inputs />
+        </div>
     </div>
 </template>
 
 <script>
+import Navigation from './Navigation.vue'
+import Inputs from './Inputs.vue'
+
     export default {
-        name: "input-components"
+        name: "input-components",
+        components: {
+            Navigation,
+            Inputs
+        }
     }
 </script>
 
-<style scoped>
+<style>
 
 </style>
