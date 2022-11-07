67c8dc7c76b9ef75c1d133d25e54cb26706f46bd
hasanaimroatun <imroatunhasana@gmail.com>
2022-11-07 19:31:00 +0700

    import faPhone and faLock then add them to library

--- src/main.js
+++ src/main.js
@@ -4,9 +4,10 @@ import "../node_modules/bootstrap/dist/css/bootstrap.css"
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle"
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faCartPlus } from '@fortawesome/free-solid-svg-icons'
+import { faPhone, faLock } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faCartPlus)
+library.add(faPhone)
+library.add(faLock)
 
 
 createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
