8e25eedf8aeb8b9eefac387dd2321c9d0d36580d
hasanaimroatun <imroatunhasana@gmail.com>
2022-11-09 13:33:10 +0700

    import dom and add dom.watch( )

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import "../node_modules/bootstrap/dist/css/bootstrap.css"
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle"
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faPhone, faLock } from '@fortawesome/free-solid-svg-icons'
+import { faPhone, faLock, faHouse, faEnvelope, faUser, faLocationDot, faMagnifyingGlass, faCheck, faArrowRight, faArrowUp } from '@fortawesome/free-solid-svg-icons'
+import { dom } from '@fortawesome/fontawesome-svg-core'
 
-library.add(faPhone)
-library.add(faLock)
+
+library.add({faPhone, faLock, faHouse, faEnvelope, faUser, faLocationDot, faMagnifyingGlass, faCheck, faArrowRight, faArrowUp})
 
 
 createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
+dom.watch()
